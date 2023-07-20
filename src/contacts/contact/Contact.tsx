2d8ed3638876bd9bc7b7775cea1b2dde29a39d63
nargiz92 <ada.tag.92@mail.ru>
2023-07-20 13:07:32 +0300

    fit:add onChange for Contact.tsx

--- src/contacts/contact/Contact.tsx
+++ src/contacts/contact/Contact.tsx
@@ -1,29 +1,67 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './Contact.module.scss';
+import axios from "axios";
+
+interface FormDataType {
+    name: string,
+    contact: string,
+    message: string
+}
 
 
+
+const api = {
+    login() {
+
+    }
+}
 const Contact = () => {
-    const [isFul, setIsFul]=useState()
+    const formData: FormDataType = {name: "", contact: "", message: ""}
+
+    const [responseBody, setResponseBody] = useState<FormDataType>(formData)
+
+    const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        const {name, value} = event.target
+        setResponseBody({...responseBody, [name]: value})
+
+    }
+    const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+           axios.post('https://gmail-smtp-sigma.vercel.app/sendMessage', {
+               name: responseBody.name,
+               contact: responseBody.contact,
+               message: responseBody.message
+           }).then(() => {
+               alert('success')
+           })
+
+
+    }
+
     return (
         <div className={s.colMd}>
             <div className={s.contactForms}>
-                <form className={s.inputContacts}>
+                <form className={s.inputContacts} id={'contact-form'} onSubmit={onSubmitHandler}>
                     <div className={s.row}>
                         <div className={s.nameInput}>
-                            <input className={`${s.mesInp}`}/>
+                            <input className={`${s.mesInp}  ${responseBody.name.length?s.isFull:''}`} name={'name'} onChange={(e) => inputChangeHandler(e)}
+                                   type="text"/>
                             {/*<input className={`${s.mesInp} ${value?s.isFull:''}`}/>*/}
                             <div className={s.helpName}></div>
-                            <span>Name *</span>
+                            <span className={`${responseBody?s.isFul:s.orderSpan}`}>Name *</span>
                         </div>
                         <div className={s.emailInput}>
-                            <input className={s.email}/>
+                            <input className={`&{s.email}  ${responseBody.contact.length?s.isFull:''}`} name={'contact'} onChange={(e) => inputChangeHandler(e)}
+                                   type="text"/>
                             <div className={s.helpName}></div>
                             <span>Email *</span>
                         </div>
                     </div>
                     <div className={s.messageTextareaAndButton}>
-                        <div  className={s.textareaGroup}>
-                            <textarea className={s.textAreaForm}/>
+                        <div className={s.textareaGroup}>
+                            <textarea className={`${s.textAreaForm}  ${responseBody.message.length?s.isHaveValue:''}`} name={'message'}
+                                      onChange={(e) => inputChangeHandler(e)}/>
                             <div className={s.helpName}>
                             </div>
                             <span>Message *</span>
@@ -34,7 +72,6 @@ const Contact = () => {
                             </div>
                         </div>
                     </div>
-
 
 
                 </form>
